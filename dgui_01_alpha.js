5a29db1b6487f005fe009ec43a21e0a19867de0c
Sergey Zolotykh <milk3dfx@gmail.com>
2012-07-15 19:39:16 -0400

    version 2

--- dgui_01_alpha.js
+++ dgui_01_alpha.js
@@ -8,11 +8,13 @@ gui_drag_counter = 0;
 
 MouseX = 0;
 MouseY = 0;
-MouseX_old = 0;
-MouseY_old = 0;
+
+GUI=new Object();
 
 //Константы
-gui_speed_move = 20;
+DGUI_SPEED_MOVE = 20;
+DGUI_BROWSER = DGetBrowser();
+
 
 
 ////////////////КЛАССЫ////////////////////
@@ -20,17 +22,23 @@ gui_speed_move = 20;
 //Класс фрема
 function TFrame() 
 {
-    this.width;
-    this.height;
-    this.parent_frame;
-    this.order = 0;
     this.ent;
-    this.x = 0;
-    this.y = 0;
     this.act = true;
     this.color;
     this.alpha = 1.0;
 	this.mouseover = false;
+	
+	this.DGetXFrame = DGetXFrame;
+	this.DGetYFrame = DGetYFrame;
+	this.DGetWidthFrame = DGetWidthFrame;
+	this.DGetHeightFrame = DGetHeightFrame;
+	this.DSetXFrame = DSetXFrame;
+	this.DSetYFrame = DSetYFrame;
+	this.DSetWidthFrame = DSetWidthFrame;
+	this.DSetHeightFrame = DSetHeightFrame;
+	this.DGetZFrame = DGetZFrame;
+	this.DSetZFrame = DSetZFrame;
+	
 	
     this.DCreateFrame = DCreateFrame;
 	this.DSizeFrame = DSizeFrame;
@@ -39,6 +47,11 @@ function TFrame()
 	this.DAlphaFrame = DAlphaFrame;
 	this.DColorFrame = DColorFrame;
 	this.DMoveFrame = DMoveFrame;
+	this.DImageToFrame = DImageToFrame;
+	//---------------24.01.2009------------
+	this.DSetClassName = DSetClassName;
+	this.DGetClassName = DGetClassName;
+	//-------------------------------------
 }
 
 //Класс текста
@@ -57,32 +70,50 @@ function TText()
 
 /////////////////ВСПОМОГАТЕЛЬЫЕ ФУНКЦИИ//////////////////
 
+//Получение типа используемого браузера
+function DGetBrowser()
+{
+	// Получим userAgent браузера и переведем его в нижний регистр
+	var ua = navigator.userAgent.toLowerCase();
+	var browser_tmp;
+	
+	// Определим Internet Explorer<span id="more-46"></span>
+	if ((ua.indexOf("msie") != -1) && (ua.indexOf("opera") == -1) && (ua.indexOf("webtv") == -1)) browser_tmp = "IE";
+	// Opera
+	if(ua.indexOf("opera") != -1) browser_tmp = "Opera";
+	// Gecko = Mozilla + Firefox + Netscape
+	if(ua.indexOf("gecko") != -1) browser_tmp = "Gecko";
+	// Safari, используется в MAC OS
+	if( (ua.indexOf("safari") != -1)) browser_tmp = "Safari";
+	// Konqueror, используется в UNIX-системах
+	if(ua.indexOf("konqueror") != -1) browser_tmp = "Konqueror";
+	
+	return browser_tmp;
+}
 
 
 /////////////////БАЗОВЫЕ ФУНКЦИИ//////////////////
+
+
 
 //Создание основы окна (фрейм)
 function DCreateFrame(width, height, color)
 {
     
-    this.width = width;
-    this.height = height;
-    this.x = 0;
-    this.y = 0;
     this.color = color;
     
     gui_frame_id++;
     this.id = "frame_" + gui_frame_id;
     
     gui_order++;
-    this.order = gui_order;
     
-    this.ent = document.createElement("div");
+
+	this.ent = document.createElement("div");
     this.ent.id = this.id;
-    this.ent.style.width = this.width + "px";
-    this.ent.style.height = this.height + "px";
-    this.ent.style.zIndex = this.order;
+    this.ent.style.zIndex = gui_order;
     this.ent.style.position = "absolute";
+	this.ent.style.width = width + "px";
+    this.ent.style.height = height + "px"; 
     this.ent.style.backgroundColor = color;
     this.ent.style.left = "0px";
     this.ent.style.top = "0px";
@@ -97,84 +128,134 @@ function DCreateFrame(width, height, color)
 	//	e.offsetX=parseInt(this.style.left)+e.offsetX;
 	//	e.offsetY=parseInt(this.style.top)+e.offsetY;
 	};
-	this.parent_frm = document.body;
+	
+	this.OnStartDrag = function(){};
+	this.OnStopDrag = function(){};
+	this.OnDrag = function(){};
+	GUI[this.id]=this;
 	document.body.appendChild(this.ent);
 }
 
+
+//Значение X координаты фрейма
+function DGetXFrame(){
+	return 	parseInt(this.ent.style.left);
+}
+//Значение Y координаты фрейма
+function DGetYFrame(){
+	return 	parseInt(this.ent.style.top);
+}
+//Значение ширины/длины фрейма
+function DGetWidthFrame(){
+	return 	parseInt(this.ent.style.width);
+}
+//Значение высоты фрейма
+function DGetHeightFrame(){
+	return 	parseInt(this.ent.style.height);
+}
+//Значение по оси Z фрейма
+function DGetZFrame(){
+	return 	parseInt(this.ent.style.zIndex);
+}
+//Устанавливаем X координату фрейма
+function DSetXFrame(x){
+	this.ent.style.left = x;
+}
+//Устанавливаем Y координату фрейма
+function DSetYFrame(y){
+	this.ent.style.top = y;
+}
+//Значение ширины/длины фрейма
+function DSetWidthFrame(width){
+	this.ent.style.width = width;
+}
+//Значение высоты фрейма
+function DSetHeightFrame(height){
+	this.ent.style.height = height;
+}
+//Значение по оси Z фрейма
+function DSetZFrame(zindex){
+	this.ent.style.zIndex = zindex;
+}
+
 //Изменение размера фрейма
-function DSizeFrame(width, height)
-{
-	this.width = width;
-	this.height = height;
-	
-	this.ent.style.width = this.width;
-	this.ent.style.height = this.height;
+function DSizeFrame(width, height, inc){
+	this.ent.style.width = width + inc;
+	this.ent.style.height = height + inc;
 }
 
 //Изменение видимости фрейма
-function DHideFrame(bool)
-{
-
-	if(bool == true)
-	{
+function DHideFrame(bool){
+	if(bool == "undefened") bool = true;
+	
+	if(bool == true){
 		this.act = false;
 		this.ent.style.display = "none";
 	}
-	else
-	{
+	else{
 		this.act = true;
 		this.ent.style.display = "block";
 	}
 }
 
 //Прозрачность фрейма
-function DAlphaFrame(alpha)
-{
-	this.alpha = alpha/100.0;
+function DAlphaFrame(alpha){
+	alpha_ = alpha/100.0;
 	//Для Opera, Mozilla
-	this.ent.style.opacity = this.alpha;	
+	this.ent.style.opacity = alpha_;
 	//Для IE
 	this.ent.style.filter += "progid:DXImageTransform.Microsoft.Alpha(opacity="+alpha+")";
 }
 
 //Цвет фрейма
-function DColorFrame(color)
-{
-	this.color = color;
+function DColorFrame(color){
 	this.ent.style.backgroundColor = color;
 }
 
 //Изменение положения фрейма
-function DPositionFrame(x, y)
-{
+function DPositionFrame(x, y){
 	this.ent.style.left = x+"px";
 	this.ent.style.top = y+"px";
-	
-	this.x = x;
-	this.y = y;
 }
 
 //Движение фрейма
-function DMoveFrame(movex, movey)
-{
-	x = movex + this.x;
-	y = movey + this.y;
+function DMoveFrame(movex, movey){
+	x = movex + this.DGetXFrame();
+	y = movey + this.DGetYFrame();
 	this.DPositionFrame(x, y);
 }
 
-//Обработка drag-and-drop фрейма frm
-function DDragFrame(frm, met)//старое насвание DOnMouseFrame
+function DImageToFrame(image_src, repeat_bool)
 {
+	var repeat = "no-repeat";
+	if(!repeat_bool) repeat = "no-repeat";
+	if(repeat_bool) repeat = "repeat";
+
+	this.ent.style.backgroundImage = "url(" + image_src + ")";
+	this.ent.style.backgroundRepeat = repeat; //-----"repeat" "no-repeat"
+}
+
+//---------------24.01.2009------------
+//Устанавливаем className для фрейма
+function DSetClassName(className)
+{
+	this.ent.className = className;
+}
+//Получаем className фрейма
+function DGetClassName()
+{
+	return this.ent.className;
+}
+//-------------------------------------
+//Обработка drag-and-drop фрейма frm
+function DDragFrame(frm, met){//старое насвание DOnMouseFram{
 	//met = none, never, drag;
 	el = document.getElementById(frm.id);
 	
 	el.drag=met;
 	
-	if (el.drag == "none")
-	{
+	if (el.drag == "none"){
 		el.onmousedown = function(e){};
-		document.onmouseup = function(){};
-		document.onmousemove = function(e){};
 	}
 	
 	if (el.drag == "never"){//milk: Эта опция не совсем корректно работает, жду подтверждения идеи и тогда будем
@@ -184,70 +265,70 @@ function DDragFrame(frm, met)//старое насвание DOnMouseFrame
 			if(e.stopPropagation) e.stopPropagation(); else e.cancelBubble = true;
  			if(e.preventDefault) e.preventDefault(); else e.returnValue = false;
 		};
-		document.onmouseup = function(){};
-		document.onmousemove = function(e){};
 	}
 	
 	if(el.drag == "drag"){
 		el.onmousedown = function(e){
 			e = e||event;	
-			//alert(this.id+" - "+e.offsetX+" - "+e.offsetY);
-			//gui_drag = [ this, document.body.scrollLeft+(e.x || e.clientX)-parseInt(this.style.left), 
-			//	document.body.scrollLeft+(e.y || e.clientY)-parseInt(this.style.top)];
 			gui_drag = [ this, (e.x || e.clientX)-parseInt(this.style.left), 
-				(e.y||e.clientY)-parseInt(this.style.top)];
-			//alert(this.id+" - "+gui_drag[1]+" - "+gui_drag[2]);
+				(e.y||e.clientY)-parseInt(this.style.top),"start"];
 			if(e.stopPropagation) e.stopPropagation(); else e.cancelBubble = true;
  			if(e.preventDefault) e.preventDefault(); else e.returnValue = false;
 		}
+	}
 	
-		document.onmouseup = function(){
+	//Milk: Инициализация этих 2х событий должна прохадить во время или после создания докумета,но до использования функции drag
+	document.onmouseup = function(){
+		if(gui_drag){
+		    GUI[gui_drag[0].id].OnStopDrag();
 			gui_drag = false;
 		}
-	
-		document.onmousemove = function(e){
-			e = e || event;   
-			
-			//MouseX = document.body.scrollLeft + ( e.x || e.clientX );
-			//MouseY = document.body.scrollTop + ( e.y || e.clientY );
-			
-			MouseX = e.x || e.clientX;
-			MouseY = e.y || e.clientY;
+	}
+	document.onmousemove = function(e){
+		if(gui_drag){
+		  e = e || event;
+		  MouseX = e.x || e.clientX;
+		  MouseY = e.y || e.clientY;
    	    
-			gui_drag[0].style.left = MouseX - gui_drag[1] + "px";
-			gui_drag[0].style.top = MouseY - gui_drag[2] + "px";
-	
-			frm.x = MouseX - gui_drag[1];
-			frm.y = MouseY - gui_drag[2];
+		  if(gui_drag[3]=="start"){
+			gui_drag[3]=="process";
+			GUI[gui_drag[0].id].OnStartDrag();
+		  }
 		
-			if(e.stopPropagation) e.stopPropagation();	else e.cancelBubble = true;
-			if(e.preventDefault) e.preventDefault();	else e.returnValue = false;
-	   }
+		  gui_drag[0].style.left = MouseX - gui_drag[1] + "px";
+		  gui_drag[0].style.top = MouseY - gui_drag[2] + "px";
+		  
+		  GUI[gui_drag[0].id].OnDrag();
+	
+		  if(e.stopPropagation) e.stopPropagation();	else e.cancelBubble = true;
+		  if(e.preventDefault) e.preventDefault();	else e.returnValue = false;
+		}
 	}
 }
 
-function DParentFrame(frm, parent_frm)
-{
-	//frm.ent=frm.parent_frm.removeChild(frm.ent);
+function DParentFrame(frm, parent_frm){
 	parent_frm.ent.appendChild(frm.ent);
-	frm.parent_frm = parent_frm;
-	//parent_frm.child_frm = frm;
 }
+
+
+
 
 /////////////////ОСНОВНЫЕ ФУНКЦИИ//////////////////
 
 //Создание текста
-function DCreateText(x ,y , size, text, ent_parent)
+function DCreateText(x ,y , size, text, color, color_frame, ent_parent)
 {
-    var a_tmp = document.createElement('a');
-    a_tmp.style.fontSize = size;
-	a_tmp.innerHTML = text;
-    document.body.appendChild( a_tmp );
-    var width = a_tmp.offsetWidth;
-	var height = a_tmp.offsetHeight;
-    document.body.removeChild( a_tmp );
+    //var a_tmp = document.createElement('a');
+    //a_tmp.style.fontSize = size;
+	//a_tmp.innerHTML = text;
+    //document.body.appendChild( a_tmp );
+    //var width = a_tmp.offsetWidth;
+	//var height = a_tmp.offsetHeight;
+    //document.body.removeChild( a_tmp );
 	
-	this.frm.DCreateFrame(width, height,"#99ff99");
+	//this.frm.DCreateFrame(width, height, color_frame);
+	this.frm.DCreateFrame(120, 40, color_frame);
+	this.frm.ent.style.color = color;
 	this.frm.DPositionFrame(x, y);
 	this.frm.ent.style.fontSize = size + "px";
 	this.frm.ent.innerHTML = text;
